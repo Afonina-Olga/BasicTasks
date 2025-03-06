4e51eaaf57b0c2dc2611dd5449a26033c71ba682
Afonina Olga <olga.g.afonina@gmail.com>
2025-03-06 23:07:58 +0300

    Добавьте файлы проекта.

---
+++ Quiz/Program.cs
@@ -0,0 +1,36 @@
+﻿// https://kupidonia.ru/viktoriny/viktorina-geroi-multfilmov
+var result = 0;
+var count = 2;
+
+Console.WriteLine(new string('-', 60));
+Console.WriteLine("1. Какой герой мультфильма испугался Того-кто-сидит-в-пруду?");
+Console.WriteLine("1. Крошка енот");
+Console.WriteLine("2. Котенок Гав");
+Console.WriteLine("3. Голубой щенок");
+Console.WriteLine();
+Console.Write("Ваш ответ: ");
+
+var answer1 = Console.ReadLine();
+if (answer1 == "1")
+{
+	result++;
+}
+
+Console.WriteLine();
+
+Console.WriteLine(new string('-', 60));
+Console.WriteLine("2. Какой герой мультфильма назвал себя «лучшим в мире привидением с мотором»?");
+Console.WriteLine("1. Крокодил Гена");
+Console.WriteLine("2. Кентервильское привидение");
+Console.WriteLine("3. Карлсон");
+Console.WriteLine();
+Console.Write("Ваш ответ:");
+
+var answer2 = Console.ReadLine();
+if (answer2 == "3")
+{
+	result++;
+}
+
+Console.WriteLine($"Вы ответили верно на {result} вопросов из {count}");
+
