a1d8ee60d4f0eb1f5d196f76d50a11c61aad509c
Afonina Olga <olga.g.afonina@gmail.com>
2025-03-07 23:11:05 +0300

    Частное и остаток

---
+++ FuelConsumptionCalculator/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Простой калькулятор расхода топлива");
+
+Console.Write("Введите расстояние (км):");
+var distance = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Потрачено топлива (л):");
+var vol = Convert.ToDouble(Console.ReadLine());
+
+var cons = (100 * vol) / distance;
+
+Console.WriteLine($"Автомобиль прошел {distance} км, потратил {vol} литров топлива. Средний расход - {cons} л / 100 км");
---
+++ QuotientAndRemainder/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите делимое: ");
+var divident = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите делитель: ");
+var divisor = Convert.ToDouble(Console.ReadLine());
+var quotient = divident / divisor;
+var remainder = divident % divisor;
+Console.WriteLine($"Частное {quotient} Остаток {remainder}");
