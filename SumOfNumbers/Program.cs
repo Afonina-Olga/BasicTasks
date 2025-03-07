2b7f064aa90e6bc1b8a48ff80129a33f16e56a07
Afonina Olga <olga.g.afonina@gmail.com>
2025-03-08 01:26:56 +0300

    Сумма натуральных чисел

---
+++ SumOfNumbers/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.Write("Введите положительное целое число: ");
+var number = int.Parse(Console.ReadLine()!);
+
+if (number <= 0)
+{
+	Console.WriteLine("Ну положительное же число!");
+	return;
+}
+
+var sum = 0;
+
+#region for
+
+for (int i = 0; i <= number; i++)
+{
+	sum += i;
+}
+
+Console.WriteLine($"Сумма = {sum}");
+
+#endregion
+
+
+#region While
+
+// Удобно использовать, если мы не знаем точно количество итераций
+
+sum = 0;
+var j = 0;
+
+while (j <= number)
+{
+	sum += j;
+	j++;
+}
+
+Console.WriteLine($"Сумма = {sum}");
+
+#endregion
