2b34a0a50978c8ae54b8ce5d4a18c02de49be0c5
Afonina Olga <olga.g.afonina@gmail.com>
2025-03-08 01:37:52 +0300

    Нерекурсивный факториал

---
+++ FactorialNonRecursive/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите положительное целое число: ");
+ulong number = Convert.ToUInt64(Console.ReadLine());
+ulong factorial = 1;
+
+for (ulong i = 1; i <= number; i++)
+{
+	factorial *= i;
+}
+
+Console.WriteLine($"Факториал = {factorial}");
+// Заодно показать как объявлять переменные, почему тут ulong
