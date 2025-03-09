fb47ca0c12086d5962527e1647b623634a8e4927
Afonina Olga <olga.g.afonina@gmail.com>
2025-03-09 13:45:20 +0300

    Подсчет цифр в числе

---
+++ NumbersCount/Program.cs
@@ -0,0 +1,20 @@
+﻿// Подсчет цифр в числе
+
+// Можно сделать совсем просто, но наша цель изучать алгоритмы
+Console.Write("Введите целое число: ");
+var input = Console.ReadLine();
+
+var number = long.Parse(input);
+
+var count = 0;
+
+while (number != 0)
+{
+	number /= 10;
+	count++;
+}
+
+Console.WriteLine($"Количество цифр = {count}");
+
+// Вариант совсем простой
+Console.WriteLine($"Количество цифр = {input.Length}");
