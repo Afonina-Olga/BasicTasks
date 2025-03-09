c78da80a8df02064bf43b78286b257c089b48e22
Afonina Olga <olga.g.afonina@gmail.com>
2025-03-09 13:59:56 +0300

    Палиндром

---
+++ Palindrome/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine(IsPalindrome("1234"));
+Console.WriteLine(IsPalindrome("12345"));
+Console.WriteLine(IsPalindrome("1221"));
+Console.WriteLine(IsPalindrome("12321"));
+
+static bool IsPalindrome(string value)
+{
+	for (int i = 0; i < value.Length / 2; i++) // Если не делить на 2, то ошибки не будет, но будут лишние действия
+	{
+		if (value[i] != value[value.Length - i - 1]) 
+		{
+			return false;
+		}
+	}
+	return true;
+}
---
+++ ReverseNumber/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите целое число: ");
+var number = long.Parse(Console.ReadLine());
+
+// 1. Вычислить остаток от деления на 10
+// 2. Постепенно формируем обратное число - постепенно умножаем его на 10 и
+// добавляем остаток от предыдущей операции деления
+
+var result = 0L;
+
+while (number != 0)
+{
+	var remainder = number % 10;
+	result = result * 10 + remainder;
+	number /= 10;
+}
+
+Console.WriteLine($"Обратное число = {result}");
+
+// Способ с массивами показать
