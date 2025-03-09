c7272c9757074fbc3153869dff4aa76d6316595a
Afonina Olga <olga.g.afonina@gmail.com>
2025-03-09 14:25:03 +0300

    Простой калькулятор

---
+++ SimpleCalculator/Program.cs
@@ -0,0 +1,20 @@
+﻿// Простой калькулятор с case
+
+Console.Write("Введите первое число: ");
+var number1 = double.Parse(Console.ReadLine());
+
+Console.Write("Введите операцию (+, -, /, *) ");
+var operation = Console.ReadLine();
+
+Console.Write("Введите второе число: ");
+var number2 = double.Parse(Console.ReadLine());
+
+switch (operation)
+{
+	case "+":
+		break;
+
+	default:
+		Console.WriteLine("Такая операция не реализована");
+		break;
+}
