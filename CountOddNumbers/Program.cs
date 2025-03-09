572846537c279a339837ff243b343b86d877491f
Afonina Olga <olga.g.afonina@gmail.com>
2025-03-09 12:56:19 +0300

    Наибольший общий делитель

---
+++ CountOddNumbers/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите максимальное число: "); // До которого выводить
+var number = int.Parse(Console.ReadLine()!);
+if (number < 0)
+{
+	Console.WriteLine("Число должно быть положительным");
+	return;
+}
+
+for (int i = 1; i <= number; i++) // До number включительно
+{
+	if (i % 2 == 0)
+	{
+		continue;
+	}
+
+	Console.WriteLine(i);
+}
+
---
+++ GreatestCommonDivisor/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите первое число: ");
+var number1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+var number2 = int.Parse(Console.ReadLine());
+
+int gcd = 0;
+
+for (int i = 1; i < Math.Max(number1, number2); i++)
+{
+	if (number1 % i == 0 && number2 % i == 0)
+	{
+		gcd = i;
+	}
+}
+
+Console.WriteLine($"Наибольший общий делитель: {gcd}");
