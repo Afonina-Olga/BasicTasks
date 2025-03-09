3c419e8c4dbfec3f77418e4f72b4f1a237c2c451
Afonina Olga <olga.g.afonina@gmail.com>
2025-03-09 14:19:29 +0300

    Является ли число простым

---
+++ SimpleDigit/Program.cs
@@ -0,0 +1,29 @@
+﻿// Простое число делится только на 1 и на себя
+
+Console.Write("Введите целое положительное число");
+var n = int.Parse(Console.ReadLine());
+
+bool IsPrime(int value)
+{
+	for (int i = 2; i <= value / 2; i++)
+	{
+		if (value % i == 0)
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+// Показать как без метода
+// Насколько метод удобнее
+
+var isPrime = true;
+for (int i = 2; i <= n / 2; i++)
+{
+	if (n % i == 0)
+	{
+		isPrime = false;
+		break;
+	}
+}
